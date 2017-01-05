9fb15f138f7dca32ddf3f6387b4deb75b138fca9
dvopalecky <daniel.vopalecky@gmail.com>
2017-01-05 21:38:17 +0000

    Add extra features to TicTacToe

--- lesson_5/tictactoe.rb
+++ lesson_5/tictactoe.rb
@@ -1,4 +1,15 @@
 # Tic Tac Toe in OOP
+require 'pry'
+
+class Array
+  def joinor(delimiter = ', ', last_word = 'or')
+    if size < 2
+      join(delimiter)
+    else
+      self[0...-1].join(delimiter) + "#{delimiter}#{last_word} #{self[-1]}"
+    end
+  end
+end
 
 class String
   # colorization
@@ -34,6 +45,20 @@ end
 class Helpers
   def self.clear_screen
     system('clear') || system('cls')
+  end
+
+  def self.display_and_choose_menu(heading, integer_choices)
+    clear_screen
+    puts heading
+    integer_choices.each do |key, choice|
+      puts "(#{key}) #{choice}"
+    end
+
+    loop do
+      answer = gets.chomp.to_i
+      return answer if integer_choices.keys.include?(answer)
+      puts "Invalid choice. Please choose #{integer_choices.keys.to_a.joinor}"
+    end
   end
 end
 
@@ -72,28 +97,27 @@ end
 class Player
   attr_reader :name, :marker
   attr_accessor :score
-
-  def initialize
-    @score = 0
-  end
 end
 
 class Human < Player
+  attr_accessor :name
   MARKER = Square.new("X", :red)
 
   def initialize
-    @name = "Human"
+    @name = "Player"
     @marker = MARKER
   end
 
   def mark_square(board)
     square = nil
     loop do
-      puts "Select square (#{board.unmarked_keys.join(', ')})"
+      puts "Select square (#{board.unmarked_keys.joinor(', ')})"
       square = gets.chomp.to_i
       break if board.unmarked_keys.include?(square)
+
       puts "Sorry, that's not a valid choice"
     end
+
     board[square] = marker
   end
 end
@@ -107,7 +131,11 @@ class Computer < Player
   end
 
   def mark_square(board)
-    square = board.unmarked_keys.sample
+    square = board.find_winning_square(Computer::MARKER) # offense
+    square = board.find_winning_square(Human::MARKER) if !square # offense
+    square = 5 if !square && board.unmarked_keys.include?(5)
+    square = board.unmarked_keys.sample if !square
+
     board[square] = marker
   end
 end
@@ -140,7 +168,7 @@ class Board
     @squares.select { |_, square| square.unmarked? }.keys
   end
 
-  def game_over?
+  def round_over?
     full? || winning_marker
   end
 
@@ -155,6 +183,7 @@ class Board
         return squares.first
       end
     end
+
     nil
   end
 
@@ -166,7 +195,25 @@ class Board
     end
   end
 
+  def find_winning_square(offensive_marker)
+    WINNING_LINES.each do |line| # try to win in 1 move
+      next unless count_markers_on_line(line, offensive_marker) == 2
+      selected = @squares.select do |k, v|
+        line.include?(k) && v == @empty_square
+      end
+
+      square = selected.keys.first
+      return square unless square.nil?
+    end
+
+    nil
+  end
+
   private
+
+  def count_markers_on_line(line, marker)
+    @squares.values_at(*line).count(marker)
+  end
 
   def display_dashes
     puts "-------------"
@@ -177,6 +224,7 @@ class Board
     row.each do |key|
       message += " #{@squares[key]} |"
     end
+
     puts message
   end
 
@@ -187,6 +235,77 @@ class Board
   end
 end
 
+class TTTGameSettings
+  POINTS_TO_WIN = 2
+
+  attr_accessor :points_to_win, :who_goes_first
+
+  def initialize
+    @points_to_win = POINTS_TO_WIN
+    @who_goes_first = :player
+  end
+
+  def change(human)
+    loop do
+      heading = "Tic Tac Toe: Settings"
+      menu = { 1 => "Points to win    | #{points_to_win}",
+               2 => "Who goes first   | #{who_goes_first.capitalize}",
+               3 => "Player name      | #{human.name}",
+               4 => "Player symbol    | #{human.marker}",
+               5 => "Exit to main menu" }
+      case Helpers.display_and_choose_menu(heading, menu)
+      when 1 then @points_to_win = change_points_to_win
+      when 2 then @who_goes_first = change_who_goes_first
+      when 3 then human.name = change_player_name
+      when 4 then human.marker.marker = change_player_marker
+      else break
+      end
+    end
+  end
+
+  private
+
+  def change_points_to_win
+    puts "How many points to win? (1 to 10)"
+    loop do
+      answer = gets.chomp.to_i
+      return answer if (1..10).cover?(answer)
+      puts "Invalid input. Enter 1 to 10."
+    end
+  end
+
+  def change_who_goes_first
+    heading = "Who goes first"
+    menu = { 1 => "Player",
+             2 => "Computer",
+             3 => "Switch each round, first random" }
+    case Helpers.display_and_choose_menu(heading, menu)
+    when 1 then :player
+    when 2 then :computer
+    when 3 then :random
+    end
+  end
+
+  def change_player_name
+    puts "What is your name?"
+    loop do
+      answer = gets.chomp
+      return answer unless answer.empty?
+      puts "Input can't be empty. Try again."
+    end
+  end
+
+  def change_player_marker
+    puts "What symbol do you want? (single character)"
+    loop do
+      answer = gets.chomp
+      return answer if answer.size == 1 && answer != Computer::MARKER.marker
+      puts "Input must have 1 character and must not be "\
+           "#{Computer::MARKER.marker}. Try again."
+    end
+  end
+end
+
 class TTTGame
   attr_accessor :board, :human, :computer
 
@@ -194,80 +313,138 @@ class TTTGame
     @board = Board.new
     @human = Human.new
     @computer = Computer.new
+    @settings = TTTGameSettings.new
+    @player = nil
+    @who_starts_round = nil
+    @is_first_round = nil
   end
 
-  # rubocop:disable Metrics/MethodLength
-  def play
-    display_welcome_message
+  def main
+    heading = "Tic Tac Toe: Main Menu"
+    menu = { 1 => "Play", 2 => "Settings", 3 => "Exit game" }
     loop do
-      @player = @human # who starts
-      display_board
-      loop do
-        @player.mark_square(@board)
-        clear_screen_and_display_board
-        switch_player
-        break if @board.game_over?
+      case Helpers.display_and_choose_menu(heading, menu)
+      when 1 then play
+      when 2 then @settings.change(@human)
+      else break
       end
-      display_result
-      break unless play_again?
-      @board.reset
-      display_play_again_message
     end
+
     display_goodbye_message
   end
-  # rubocop:enable Metrics/MethodLength
 
   private
+
+  def play
+    @is_first_round = true
+    reset_score
+    loop do
+      play_round
+      display_round_result
+      break if game_over?
+
+      press_any_key
+    end
+
+    display_game_result
+    press_any_key
+  end
+
+  def press_any_key
+    puts "Press enter to continue."
+    gets
+  end
+
+  def play_round
+    set_who_starts_round
+    @player = @who_starts_round
+    @board.reset
+    loop do
+      clear_screen_and_display_score_and_board
+      @player.mark_square(@board)
+      switch_player
+      break if @board.round_over?
+    end
+
+    @is_first_round = false
+    update_score
+    clear_screen_and_display_score_and_board
+  end
+
+  def set_who_starts_round
+    case @settings.who_goes_first
+    when :player
+      @who_starts_round = @human
+    when :computer
+      @who_starts_round = @computer
+    when :random
+      if @is_first_round
+        @who_starts_round = [@human, @computer].sample
+      else
+        @who_starts_round = @who_starts_round == @human ? @computer : @human
+      end
+    end
+  end
+
+  def game_over?
+    @human.score >= @settings.points_to_win ||
+      @computer.score >= @settings.points_to_win
+  end
 
   def switch_player
     @player = @player == @human ? @computer : @human
   end
 
-  def clear_screen_and_display_board
+  def clear_screen_and_display_score_and_board
     Helpers.clear_screen
-    display_board
+    display_score_and_board
   end
 
-  def display_board
+  def display_score_and_board
     puts "You are #{Human::MARKER}, computer is #{Computer::MARKER}."
+    puts "Score: #{@human.score} : #{@computer.score}"\
+         " (#{human.name} : #{computer.name})"
     @board.display
   end
 
-  def display_welcome_message
-    puts "Welcome to Tic Tac Toe"
-  end
-
   def display_goodbye_message
-    puts "Thanks for playing"
+    puts "Thanks for playing. Good bye"
   end
 
-  def display_play_again_message
-    Helpers.clear_screen
-    puts "Let's play again|"
-  end
-
-  def display_result
+  def display_round_result
+    msg = " Winner needs #{@settings.points_to_win} points."
     case @board.winning_marker
     when Human::MARKER
-      puts "You won!"
+      puts "You won the round!" + msg
     when Computer::MARKER
-      puts "Computer won!"
+      puts "Computer won the round!" + msg
     else
-      puts "It's a tie."
+      puts "This round is a tie." + msg
     end
   end
 
-  def play_again?
-    puts "Do you want to play again? (Y)es or (N)o"
-    answer = nil
-    loop do
-      answer = gets.chomp.downcase
-      break if ['y', 'n'].include?(answer)
-      puts "Please answer Y or N"
+  def display_game_result
+    if @human.score > @computer.score
+      puts "You won the game!"
+    else
+      puts "Computer won the game!"
     end
-    answer == 'y'
+  end
+
+  def update_score
+    case @board.winning_marker
+    when Human::MARKER
+      @human.score += 1
+    when Computer::MARKER
+      @computer.score += 1
+    end
+  end
+
+  def reset_score
+    @human.score = 0
+    @computer.score = 0
   end
 end
 
 game = TTTGame.new
-game.play
+game.main
